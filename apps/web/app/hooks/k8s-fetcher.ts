d681bf21e30aab8b4e0e12370280c981561c1c96
Ainvoner <shaia@monada.co>
2024-09-09 14:14:11 +0300

    wip

---
+++ apps/web/app/hooks/k8s-fetcher.ts
@@ -0,0 +1,37 @@
+export interface Params {
+  group: string;
+  version: string;
+  plural: string;
+}
+
+const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+
+export const k8sFetcher = async (params: Params) => {
+  const queryParams = new URLSearchParams({
+    group: params.group || "",
+    version: params.version || "",
+    plural: params.plural || "",
+  }).toString();
+  // Construct the full URL with query parameters
+  const url = `${SERVER_URL}/api?${queryParams}`;
+
+  try {
+    const res = await fetch(url); // Send the request to the server
+    if (!res.ok) {
+      throw new Error(`Error fetching data from server: ${res.statusText}`);
+    }
+
+    const data = await res.json();
+
+    const map: Record<string, any> = {};
+
+    data?.items.forEach((item: any) => {
+      map[`${item.metadata.namespace}/${item.metadata.name}`] = item;
+    });
+
+    return map;
+  } catch (error) {
+    console.error("Error:", error);
+    throw new Error("Failed to load data.");
+  }
+};
--- apps/web/app/routes/$group.$version.$plural.$namespace.$name.tsx
+++ apps/web/app/routes/$group.$version.$plural.$namespace.$name.tsx
@@ -1,42 +1,16 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData, useParams } from "@remix-run/react";
 import { useEffect, useMemo, useState } from "react";
-import invariant from "@remix-run/react/dist/invariant";
-
-const SERVER_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+import { k8sFetcher } from "~/hooks/k8s-fetcher";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
-  const { group, version, plural } = params;
+  const data = await k8sFetcher({
+    group: params.group || "",
+    version: params.version || "",
+    plural: params.plural || "",
+  });
 
-  // Construct query parameters if necessary (you can add more query parameters here)
-  const queryParams = new URLSearchParams({
-    group: group || "",
-    version: version || "",
-    plural: plural || "",
-  }).toString();
-
-  // Construct the full URL with query parameters
-  const url = `${SERVER_URL}/api?${queryParams}`;
-
-  try {
-    const res = await fetch(url); // Send the request to the server
-    if (!res.ok) {
-      throw new Error(`Error fetching data from server: ${res.statusText}`);
-    }
-
-    const data = await res.json();
-
-    const map: Record<string, any> = {};
-
-    data?.items.forEach((item: any) => {
-      map[`${item.metadata.namespace}/${item.metadata.name}`] = item;
-    });
-
-    return map;
-  } catch (error) {
-    console.error("Error:", error);
-    throw new Error("Failed to load data.");
-  }
+  return data;
 };
 
 export default function Index() {
