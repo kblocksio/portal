ed3a95a9c7c0c6fdf1c82261f175943e6be08e55
Ainvoner <shaia@monada.co>
2024-09-15 12:45:28 +0300

    Update use-fetch.ts

--- apps/web/app/hooks/use-fetch.ts
+++ apps/web/app/hooks/use-fetch.ts
@@ -12,38 +12,3 @@ export function useFetch<T = unknown>(
     return axios.get<T>(url, { params }).then((res) => res.data);
   });
 }
-
-// interface UseFetchResult<T> {
-//   data: T | null;
-//   error: Error | undefined;
-//   loading: boolean;
-// }
-
-// export function useFetch<T = unknown>(
-//   url: string,
-//   options?: AxiosRequestConfig,
-// ): UseFetchResult<T> {
-//   const [data, setData] = useState<T | null>(null);
-//   const [error, setError] = useState<Error | undefined>();
-//   const [loading, setLoading] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     if (!url) return; // Ensure the URL is valid before making a request
-
-//     const fetchData = async () => {
-//       try {
-//         setLoading(true);
-//         const response = await axios.get<T>(url, options); // Make request using axios
-//         setData(response.data);
-//       } catch (err) {
-//         setError(err instanceof Error ? err : new Error("An error occurred"));
-//       } finally {
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchData();
-//   }, [url]); // Re-fetch if URL or options change
-
-//   return { data, error, loading };
-// }
