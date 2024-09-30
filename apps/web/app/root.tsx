3b730172b6b997eb41a8acd8b23df030d665738f
Elad Ben-Israel <eladb@monada.co>
2024-09-30 18:20:39 +0300

    add favicon

--- apps/web/app/root.tsx
+++ apps/web/app/root.tsx
@@ -20,6 +20,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/wing.svg" />
         <Meta />
         <Links />
       </head>
