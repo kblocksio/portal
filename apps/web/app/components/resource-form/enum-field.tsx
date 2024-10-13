80127507cf0f3bb8d97a6985a6b508ccaea4764f
Ainvoner <2538825+ainvoner@users.noreply.github.com>
2024-10-13 12:38:22 +0300

    feat: image picker (#22)

--- apps/web/app/components/resource-form/enum-field.tsx
+++ apps/web/app/components/resource-form/enum-field.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
 
 interface EnumFieldProps {
@@ -5,13 +6,25 @@ interface EnumFieldProps {
   selectedValue?: string,
   onChange: (value: string) => void,
   required?: boolean;
+  disabled?: boolean;
 }
 
-export const EnumField = ({ values, selectedValue, onChange, required }: EnumFieldProps) => {
+export const EnumField = ({ values, selectedValue, onChange, required, disabled }: EnumFieldProps) => {
+
+  const [firedOnChangeForDefaultValue, setFiredOnChangeForDefaultValue] = useState(false);
+
+  useEffect(() => {
+    if (!firedOnChangeForDefaultValue) {
+      setFiredOnChangeForDefaultValue(true);
+      if (selectedValue) {
+        onChange(selectedValue);
+      }
+    }
+  }, [selectedValue]);
 
   return (
     <div className="w-full max-w-md space-y-4">
-      <Select onValueChange={onChange} value={selectedValue} required={required}>
+      <Select onValueChange={onChange} value={selectedValue} required={required} disabled={disabled}>
         <SelectTrigger>
           <SelectValue placeholder="Select a value" />
         </SelectTrigger>
--- apps/web/app/components/resource-form/image-picker-field.tsx
+++ apps/web/app/components/resource-form/image-picker-field.tsx
@@ -1,66 +1,151 @@
-import { Github, Code } from "lucide-react"
+import { Github, Code, Check } from "lucide-react"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs"
 import { Input } from "../ui/input"
-import { useState, version } from "react"
-import { Repository } from "@repo/shared"
+import { useEffect, useMemo, useState, version } from "react"
+import { ObjectMetadata, Repository } from "@repo/shared"
 import { GhRepoSelectionField } from "./gh-repo-selection-field"
 import { MarkdownWrapper } from "../markdown"
+import { Badge } from "../ui/badge"
+import { Button } from "../ui/button"
+import { EnumField } from "./enum-field"
+import { Label } from "../ui/label"
+import { useCreateResourceWizard } from "~/CreateResourceWizardContext"
 
-const snippet = (group: string, version: string, plural: string, namespace: string, name: string, field: string, image: string) => `
+const snippet = (
+  group: string,
+  version: string,
+  plural: string,
+  system: string,
+  namespace: string,
+  name: string,
+  fieldName: string,
+  imageName: string
+) => `
 \`\`\`bash
 curl \\
   -X PATCH \\
   -H "content-type: application/json" \\
   -H "Authorization: Bearer $KBLOCKS_API_TOKEN" \\
-  -d '{ "${field}": "$IMAGE" }' \\
-  https://kblocks.io/api/resources/${group}/${version}/${plural}/${namespace}/${name}
+  -d '{ "${fieldName}": "${imageName}" }' \\
+  https://kblocks.io/api/resources/${group}/${version}/${plural}/${system}/${namespace}/${name}
 \`\`\`
 `;
 
 export interface ImagePickerFieldProps {
-  group: string
-  version: string
-  plural: string
-  namespace: string
-  name: string
-  field: string
+  objectMetadata: ObjectMetadata;
+  fieldName: string
   onImageNameChange: (imageName: string) => void
-  onGithubRepoChange: (githubRepo: Repository) => void
-  onCodeSnippetSelection: () => void
 }
 
-/**
- * 3 tabs
- * gh repo selection
- * image name input field
- * code snippet input field
- */
-export const ImagePickerField = () => {
+const SetBadge = () => (
+  <Badge variant="secondary" className="text-xs ml-2">
+    <Check className="w-3 h-3 mr-1" />
+    Set
+  </Badge>
+);
+
+const IMAGE_PLACEHOLDER = "$IMAGE_PLACEHOLDER";
+
+export const ImagePickerField = ({ onImageNameChange, objectMetadata, fieldName }: ImagePickerFieldProps) => {
+  const { selectedResourceType } = useCreateResourceWizard();
+  const [selectedImageValue, setSelectedImageValue] = useState<string | null>("IMAGE_PLACEHOLDER");
+  const [selectedImageType, setSelectedImageType] = useState<"github" | "image" | "code">("code");
   const [imageName, setImageName] = useState("");
-  const [githubRepo, setGithubRepo] = useState<Repository>();
+  const [githubRepo, setGithubRepo] = useState<Repository | null>(null);
+  const [language, setLanguage] = useState("");
+  const [branch, setBranch] = useState("");
+
+  useEffect(() => {
+    onImageNameChange(selectedImageValue ?? "");
+  }, [selectedImageType]);
+
+  const getCodeSnippet = useMemo(() => {
+    return snippet(
+      selectedResourceType?.kind ?? "",
+      selectedResourceType?.version ?? "",
+      selectedResourceType?.plural ?? "",
+      objectMetadata?.system ?? "",
+      objectMetadata?.namespace ?? "",
+      objectMetadata?.name ?? "",
+      fieldName ?? "",
+      IMAGE_PLACEHOLDER
+    );
+  }, [selectedResourceType, objectMetadata, fieldName]);
+
+  const handleSetImage = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!imageName) return;
+    setSelectedImageValue(imageName);
+    setSelectedImageType("image");
+  }
+
+  const handleGithubRepoChange = (repo: Repository | null) => {
+    setGithubRepo(repo);
+    setSelectedImageType("github");
+    setSelectedImageValue(IMAGE_PLACEHOLDER);
+  }
+
+  const handleCodeSnippetSelection = () => {
+    setSelectedImageType("code");
+    setSelectedImageValue(IMAGE_PLACEHOLDER);
+  }
 
   return (
-    <Tabs defaultValue="github" className="flex w-full">
-      <TabsList className="flex flex-col w-[200px] h-full justify-start bg-white">
+    <Tabs defaultValue={selectedImageType} className="flex w-full">
+      <TabsList className="flex flex-col min-w-[220px] h-full justify-start bg-white">
         <TabsTrigger
           value="github"
           className="w-full justify-start px-4 py-2 text-left data-[state=active]:bg-gray-100 data-[state=active]:font-semibold">
           GitHub
           <Github className="w-4 h-4 ml-2" />
+          {selectedImageType === "github" &&
+            <SetBadge />
+          }
         </TabsTrigger>
-        <TabsTrigger value="image" className="w-full justify-start px-4 py-2 text-left data-[state=active]:bg-gray-100 data-[state=active]:font-semibold">Image Name</TabsTrigger>
+        <TabsTrigger
+          value="image"
+          className="w-full justify-start px-4 py-2 text-left data-[state=active]:bg-gray-100 data-[state=active]:font-semibold">
+          Image Name
+          {selectedImageType === "image" &&
+            <SetBadge />
+          }
+        </TabsTrigger>
         <TabsTrigger
           value="code"
           className="w-full justify-start px-4 py-2 text-left data-[state=active]:bg-gray-100 data-[state=active]:font-semibold">Code Snippet
           <Code className="w-4 h-4 ml-2" />
+          {selectedImageType === "code" &&
+            <SetBadge />
+          }
         </TabsTrigger>
       </TabsList>
-      <div className="flex-grow border-l w-full min-h-[160px] border-gray-200">
+      <div className="flex-grow w-full min-h-[180px] border-gray-200">
         <TabsContent value="github" className="pl-2 w-full h-full">
           <GhRepoSelectionField
-            handleOnSelection={(repo) => setGithubRepo(repo)}
+            handleOnSelection={handleGithubRepoChange}
             initialValue={githubRepo}
           />
+          <div className="flex items-center ml-2 mr-2 space-x-6 mt-2">
+            <div className="flex items-center space-x-2">
+              <Label>Branch:</Label>
+              <Input
+                disabled={selectedImageType !== "github"}
+                type="text"
+                value={branch}
+                onChange={(e) => setBranch(e.target.value)}
+              />
+            </div>
+            <div className="flex items-center space-x-2">
+              <Label>Language:</Label>
+              <EnumField
+                disabled={selectedImageType !== "github"}
+                values={mockLanguages}
+                selectedValue={language}
+                onChange={setLanguage}
+              />
+            </div>
+          </div>
         </TabsContent>
         <TabsContent value="image" className="pl-2 h-full">
           <Input
@@ -69,17 +154,30 @@ export const ImagePickerField = () => {
             value={imageName}
             onChange={(e) => setImageName(e.target.value)}
           />
+          <div>
+            <Button
+              variant="outline"
+              disabled={imageName.length === 0}
+              role="button"
+              className="mt-2"
+              onClick={handleSetImage}
+            >
+              Set Image
+            </Button>
+          </div>
         </TabsContent>
         <TabsContent value="code" className="pl-2 h-full">
           <MarkdownWrapper
-            content={snippet("group", "version", "plural", "namespace", "name", "field", imageName)}
-            onCopy={() => {
-              setImageName("IMAGE_PLACEHOLDER");
-            }}
+            content={getCodeSnippet}
+            onCopy={handleCodeSnippetSelection}
           />
         </TabsContent>
-      </div>
-    </Tabs>
+      </div >
+    </Tabs >
   )
 
-}  
+}
+
+
+
+const mockLanguages = ["python", "go", "rust", "javascript", "typescript", "java", "csharp", "kotlin", "swift", "dart", "php", "ruby"];
