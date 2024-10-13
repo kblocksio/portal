234d1311e6869572a87a00df5d6d9b0d02a58baf
Elad Ben-Israel <eladb@monada.co>
2024-10-13 08:51:12 +0300

    feat: emit error analysis to slack

--- apps/server/src/slack-notify.ts
+++ apps/server/src/slack-notify.ts
@@ -1,29 +1,69 @@
-import { parseBlockUri, WorkerEvent } from "@kblocks/api";
+import { EventReason, parseBlockUri, WorkerEvent } from "@kblocks/api";
 import { getSlackThread, setSlackThread } from "./storage";
 import { Blocks, sendSlackMessage } from "./slack-util";
 
 export async function slackNotify(event: WorkerEvent) {
-  // ignore non-lifecycle events
-  if (event.type !== "LIFECYCLE") {
+  const slackChannel = process.env.SLACK_CHANNEL ?? "kblocks-notifications";
+  const message = formatMessage(event);
+  if (!message) {
     return;
   }
 
-  const objUri = parseBlockUri(event.objUri);
-  const slackChannel = process.env.SLACK_CHANNEL ?? "kblocks-notifications";
-  
   const thread = await getSlackThread(event.objUri);
-  const message: Blocks = [
-    {
-      type: "section",
-      text: {
-        type: "mrkdwn",
-        text: `*${objUri.plural}* *${objUri.namespace}/${objUri.name}* - ${event.event.message}`,
-      },
-    },
-  ];
   
   const ctx = await sendSlackMessage(slackChannel, message, thread);
   if (ctx.ts) {
     await setSlackThread(event.objUri, ctx.ts);
+  }
+}
+
+function formatMessage(event: WorkerEvent): Blocks | undefined {
+  const objUri = parseBlockUri(event.objUri);
+  const prefix = `*${objUri.plural}* *${objUri.namespace}/${objUri.name}*`;
+
+  switch (event.type) {
+    case "LIFECYCLE":
+      const icon = () => {
+        switch (event.event.reason) {
+          case EventReason.Started:
+            return "🚀";
+          case EventReason.Succeeded:
+            return "✅";
+          case EventReason.Failed:
+            return "❌";
+          case EventReason.Resolving:
+            return "🔍";
+          case EventReason.Resolved:
+            return "🎉";
+          default:
+            return "ℹ️";
+        }
+      }
+
+      return [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `${icon()} ${prefix} - ${event.event.message}`,
+          },
+        },
+      ];
+
+
+    case "ERROR":
+      if (event.explanation) {
+        return event.explanation;
+      } else {
+        return [
+          {
+            type: "section",
+            text: { type: "mrkdwn", text: `❌ ${prefix} - ${event.message}` },
+          },
+        ];
+      }
+
+    default:
+      return undefined;
   }
 }
