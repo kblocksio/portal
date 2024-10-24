5c93618469b6bc1fef3fa68e667b81005dc5ee32
Cristian Pallarés <cristianp@wing.cloud>
2024-10-24 11:21:56 +0200

    wip

--- apps/web/src/components/projects.tsx
+++ apps/web/src/components/projects.tsx
@@ -106,7 +106,7 @@ export const Projects = (props: ProjectsProps) => {
         ),
         cell: (props) => (
           <div className="flex items-center gap-1.5">
-            <SystemBadge blockUri={props.row.original.objUri} showSystemName />
+            <SystemBadge blockUri={props.row.original.objUri} />
           </div>
         ),
         filterFn: (row, columnId, filterValue) => {
