a6759a624aa0ac45f80eb43eff4f38bc0837d5bd
Cristian Pallarés <cristianp@wing.cloud>
2024-10-30 16:41:16 +0100

    wip

--- apps/web/src/components/force-graph.tsx
+++ apps/web/src/components/force-graph.tsx
@@ -1,10 +1,8 @@
-import "@xyflow/react/dist/style.css";
 import { useEffect, useState } from "react";
 import * as d3 from "d3-force";
 import {
   ReactFlow,
   ReactFlowProvider,
-  Panel,
   useNodesState,
   useEdgesState,
   useReactFlow,
@@ -17,19 +15,48 @@ import {
   // getStraightPath,
   StraightEdge,
   Background,
-  MiniMap,
   Controls,
 } from "@xyflow/react";
-
-import type { Edge, Node } from "@xyflow/react";
-import { Button } from "./ui/button";
+import type {
+  Edge as ReactFlowEdge,
+  Node as ReactFlowNode,
+} from "@xyflow/react";
 import { cn } from "@/lib/utils";
 
-const MyNode = (props: NodeProps) => {
+import "@xyflow/react/dist/style.css";
+
+export type OwnerNodeData = {
+  // id: string;
+  name: string;
+  description: string;
+  icon: React.ReactNode;
+  size: "big" | "small";
+};
+
+export type OwnerNode = ReactFlowNode<OwnerNodeData, "node">;
+
+const OwnerNode = (props: NodeProps<OwnerNode>) => {
   return (
-    <div className={cn("relative size-8 rounded-full bg-blue-200 shadow-md")}>
+    <div
+      className={cn(
+        "relative rounded-full bg-blue-200 shadow-md",
+        props.data.size === "big" && "size-16",
+        props.data.size === "small" && "size-8",
+      )}
+    >
       <div className="absolute inset-0 flex items-center justify-around">
-        <div className="truncate text-xs">{props.id}</div>
+        {props.data.icon}
+      </div>
+
+      <div className="absolute inset-x-0 bottom-0">
+        <div className="relative">
+          <div className="absolute inset-x-0 top-0 flex flex-col items-center">
+            <div className="whitespace-nowrap text-sm">{props.data.name}</div>
+            <div className="text-muted-foreground whitespace-nowrap text-xs">
+              {props.data.description}
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="absolute inset-0 flex items-center justify-around">
@@ -81,20 +108,35 @@ const MyNode = (props: NodeProps) => {
   );
 };
 
-type NodeDatum = d3.SimulationNodeDatum & Node;
+const nodeTypes = { node: OwnerNode };
 
-const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
+const edgeTypes = { straight: StraightEdge };
+
+type NodeDatum = {
+  id: string;
+  x: number;
+  y: number;
+  reactFlowNode: OwnerNode;
+};
+
+type EdgeDatum = ReactFlowEdge;
+
+const OwnerFlow = (props: { nodes: OwnerNode[]; edges: ReactFlowEdge[] }) => {
   const [nodes, setNodes, onNodesChange] = useNodesState(props.nodes);
   const [edges, , onEdgesChange] = useEdgesState(props.edges);
 
-  const { getNodes, getEdges, fitView } = useReactFlow();
+  const { getNodes, getEdges, fitView } = useReactFlow<
+    OwnerNode,
+    ReactFlowEdge
+  >();
   const initialized = useNodesInitialized();
 
   useEffect(() => {
     const nodeDatums = getNodes().map<NodeDatum>((node) => ({
-      ...node,
+      id: node.id,
       x: node.position.x,
       y: node.position.y,
+      reactFlowNode: node,
     }));
     const edgeDatums = getEdges().map((edge) => edge);
 
@@ -104,11 +146,14 @@ const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
 
     const simulation = d3
       .forceSimulation<NodeDatum>()
-      .force("charge", d3.forceManyBody().strength(-400))
+      .force("charge", d3.forceManyBody().strength(-2000))
       .force(
         "collide",
         d3.forceCollide<NodeDatum>().radius((d) => {
-          return Math.max(d.measured?.width ?? 0, d.measured?.height ?? 0);
+          return Math.max(
+            d.reactFlowNode.measured?.width ?? 0,
+            d.reactFlowNode.measured?.height ?? 0,
+          );
         }),
       )
       .stop();
@@ -116,11 +161,11 @@ const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
     simulation.nodes(nodeDatums).force(
       "link",
       d3
-        .forceLink(edgeDatums)
+        .forceLink<NodeDatum, EdgeDatum>(edgeDatums)
         .id((d) => d.id)
         .strength((d) => {
           // console.log(d);
-          return d.source.id === "1" ? 0.35 : 1;
+          return d.source.id === "1" ? 0.295 : 0.5;
         }),
     );
 
@@ -131,25 +176,14 @@ const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
     );
     setNodes(
       nodeDatums.map((node) => ({
-        ...node,
-        position: { x: node.x, y: node.y },
+        // Careful! The `...` seems to be necessary to avoid infinite rerenders.
+        ...node.reactFlowNode,
+        position: { x: node.x ?? 0, y: node.y ?? 0 },
       })),
     );
     requestAnimationFrame(() => {
       fitView();
     });
-
-    // requestAnimationFrame(() => {
-    //   debugger;
-    //   fitView();
-    //   debugger;
-    // });
-
-    // window.addEventListener("load", () => {
-    //   requestAnimationFrame(() => {
-    //     fitView();
-    //   });
-    // });
   }, [initialized, getNodes, getEdges, setNodes, fitView]);
 
   useEffect(() => {
@@ -167,8 +201,8 @@ const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
       )}
     >
       <ReactFlow
-        nodeTypes={{ mynode: MyNode }}
-        edgeTypes={{ myedge: StraightEdge }}
+        nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
         nodes={nodes}
         edges={edges}
         onNodesChange={onNodesChange}
@@ -176,19 +210,18 @@ const ForceGraph = (props: { nodes: Node[]; edges: Edge[] }) => {
       >
         <Background />
         <Controls showInteractive={false} />
-        <MiniMap pannable zoomable />
-        {/* <Panel>
-          <Button onClick={() => fitView()}>Fit View</Button>
-        </Panel> */}
       </ReactFlow>
     </div>
   );
 };
 
-export const StoryComponent = (props: { nodes: Node[]; edges: Edge[] }) => (
+export const StoryComponent = (props: {
+  nodes: OwnerNode[];
+  edges: ReactFlowEdge[];
+}) => (
   <div className="size-[48rem]">
     <ReactFlowProvider>
-      <ForceGraph nodes={props.nodes} edges={props.edges} />
+      <OwnerFlow nodes={props.nodes} edges={props.edges} />
     </ReactFlowProvider>
   </div>
 );
