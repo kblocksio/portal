e784de4a280a95bec0ed87199efd5ee73b66f48c
Ainvoner <shaia@monada.co>
2024-10-31 20:57:32 +0200

    Update notifications-menu.tsx

--- apps/web/src/components/notifications-menu.tsx
+++ apps/web/src/components/notifications-menu.tsx
@@ -1,10 +1,11 @@
-import { useCallback, useContext, useMemo, useState } from "react";
+import { useCallback, useContext, useEffect, useMemo, useState } from "react";
 import { Bell, CheckCircle, XCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { NotificationsContext } from "@/notifications-context";
@@ -18,15 +19,15 @@ export interface NotificationMenuProps {
 
 export const NotificationMenu = ({ className }: NotificationMenuProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [hasNewNotifications, setHasNewNotifications] = useState(false);
   const { notifications, clearNotifications } =
     useContext(NotificationsContext);
 
-  const hasNotifications = useMemo(
-    () => notifications.length > 0,
-    [notifications],
-  );
+  useEffect(() => {
+    setHasNewNotifications(notifications.length > 0);
+  }, [notifications]);
 
-  const handleClose = useCallback(() => {
+  const handleClearAllNotifications = useCallback(() => {
     setIsOpen(false);
     clearNotifications();
   }, [clearNotifications]);
@@ -42,11 +43,19 @@ export const NotificationMenu = ({ className }: NotificationMenuProps) => {
 
   return (
     <div className="relative ml-auto">
-      <DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+      <DropdownMenu
+        open={isOpen}
+        onOpenChange={(open) => {
+          setIsOpen(open);
+          if (!open) {
+            setHasNewNotifications(false);
+          }
+        }}
+      >
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="relative" size="icon">
             <Bell className="h-[1.2rem] w-[1.2rem]" />
-            {hasNotifications && (
+            {hasNewNotifications && (
               <span className="absolute right-0 top-0 h-2 w-2 rounded-full bg-red-500" />
             )}
             <span className="sr-only">Toggle notifications</span>
@@ -58,7 +67,7 @@ export const NotificationMenu = ({ className }: NotificationMenuProps) => {
               {notifications.map((notification, index) => (
                 <DropdownMenuItem
                   key={`${notification.id}_${index}`}
-                  className="flex items-center space-x-2 py-2"
+                  className="flex items-center space-x-2 p-2"
                 >
                   <Link
                     to={notification.id as any}
@@ -71,11 +80,12 @@ export const NotificationMenu = ({ className }: NotificationMenuProps) => {
                   </Link>
                 </DropdownMenuItem>
               ))}
+              <DropdownMenuSeparator />
               <DropdownMenuItem asChild>
                 <Button
                   variant="ghost"
                   className="mt-2 w-full"
-                  onClick={handleClose}
+                  onClick={handleClearAllNotifications}
                 >
                   Clear all notifications
                 </Button>
