9e9e6d3c2c64dde08ae142dd7d2aadf4a512a81a
Elad Ben-Israel <eladb@monada.co>
2024-11-05 14:59:42 +0200

    restore "shell" output ui

--- apps/web/src/components/resource-outputs/components/swagger-ui.tsx
+++ apps/web/src/components/resource-outputs/components/swagger-ui.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { PropertyKey, PropertyValue } from "@/components/ui/property";
 import { SwaggerUIDialog } from "@/components/resource-outputs/components/swagger-ui-dialog";
