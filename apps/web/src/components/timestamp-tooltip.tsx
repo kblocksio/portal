5ab426806816c9e5ee4f8052b8d83faf02d85430
Cristian Pallarés <cristianp@wing.cloud>
2024-11-07 11:15:04 +0100

    wip

--- apps/web/src/components/timestamp-tooltip.tsx
+++ apps/web/src/components/timestamp-tooltip.tsx
@@ -18,7 +18,7 @@ export const TimestampTooltip = (
     <TooltipProvider delayDuration={400}>
       <Tooltip>
         <TooltipTrigger>
-          <span className="hover:bg-muted rounded-sm px-1 py-0.5 hover:bg-gray-100">
+          <span className="hover:bg-muted flex items-center rounded-sm px-1 py-0.5 hover:bg-gray-100">
             {props.children}
           </span>
         </TooltipTrigger>
