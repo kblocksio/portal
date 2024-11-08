f33cc304543fdb50aa0d87d31e75f804617da7a0
Ainvoner <shaia@monada.co>
2024-11-08 16:08:04 +0200

    wip

--- apps/web/src/components/coming-soon.tsx
+++ apps/web/src/components/coming-soon.tsx
@@ -1,9 +1,9 @@
+import { Button } from "./ui/button";
 import { SparklesCore } from "./ui/sparkles";
-import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export const ComingSoon = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center overflow-hidden bg-black">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-12 overflow-hidden bg-black">
       <div className="absolute inset-0 h-screen w-full">
         <SparklesCore
           id="tsparticlesfullpage"
@@ -15,7 +15,7 @@ export const ComingSoon = () => {
           particleColor="#FFFFFF"
         />
       </div>
-      <div className="mx-auto flex flex-col items-center justify-center">
+      <div className="mx-auto flex flex-col items-center justify-center gap-10">
         <img
           src="/wing-light.svg"
           className="text-white"
@@ -24,16 +24,25 @@ export const ComingSoon = () => {
           height={153}
         />
         <h1 className="text-center text-3xl font-bold text-white md:text-7xl lg:text-6xl">
-          Paving platform golden paths
+          Your golden path to production
         </h1>
-        <TextGenerateEffect
+        {/* <TextGenerateEffect
           className="mt-6 self-end"
           textClassName="text-white text-3xl"
           duration={4}
           filter={false}
           words={"Coming Soon..."}
-        />
+        /> */}
       </div>
+      <Button
+        variant="outline"
+        className="z-10"
+        onClick={() => {
+          window.open("https://go.kblocks.io/early-access", "_blank");
+        }}
+      >
+        Apply for early access
+      </Button>
     </div>
   );
 };
