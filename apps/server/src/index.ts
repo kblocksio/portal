c9bc3727de009dac91061806ad43edc1010aa946
Elad Cohen <eladc@monada.co>
2024-11-10 17:42:24 +0200

    chore: update blocks version

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -36,7 +36,7 @@ const port = process.env.PORT ?? 3001;
 
 const { app } = expressWs(express());
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(
   cors({
     origin: "*",
