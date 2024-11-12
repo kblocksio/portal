38b47cf60a972495190779a90cc47b3e16eeae7e
Elad Ben-Israel <eladb@monada.co>
2024-11-12 22:12:01 +0200

    auto-detect env

--- install.sh
+++ install.sh
@@ -8,19 +8,17 @@ if [ -z "$env" ]; then
   exit 1
 fi
 
-if [ "$env" == "kind" ]; then
-  kubectl config use-context kind-kind
+context=$(kubectl config current-context)
+
+if [ "$context" == "kind-kind" ]; then
   export PORTAL_INGRESS_HOST=localhost.kblocks.io
   export PORTAL_REDIS_ENABLED=true
   export VITE_BACKEND_ENDPOINT=localhost.kblocks.io
   export VITE_SKIP_AUTH=true
-  skaffold run
-elif [ "$env" == "dev" ]; then
+else
   qkube use staging.quickube.sh
   export PORTAL_INGRESS_HOST=staging.kblocks.io
   export PORTAL_REDIS_ENABLED=false
-  skaffold run
-else
-  echo "Error: Invalid env value"
-  exit 1
 fi
+
+skaffold run
