a8b6cacad5c607f03e91d7ebfa0f0ac732419440
Cristian Pallarés <cristianp@wing.cloud>
2024-11-25 15:53:01 +0100

    wip

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ context=$(kubectl config current-context)
 if [ "$context" == "kind-kind" ]; then
   export PORTAL_INGRESS_HOST=localhost.kblocks.io
   export PORTAL_REDIS_ENABLED=true
-  export VITE_BACKEND_URL=http://localhost:3001
-  export VITE_WS_URL=ws://localhost:3001/api/events
+  export VITE_BACKEND_URL=https://localhost.kblocks.io
+  export VITE_WS_URL=wss://localhost.kblocks.io/api/events
   export VITE_SKIP_AUTH=true
 else
   qkube use staging.quickube.sh
