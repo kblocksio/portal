6f99ab6019342ca6d3fba360b02d3feba29d9adb
Elad Cohen <eladc@monada.co>
2024-12-01 15:17:26 +0200

    chore: update kblocks version

--- gallery/install-blocks.sh
+++ gallery/install-blocks.sh
@@ -11,16 +11,19 @@ KBLOCKS_CLI=${KBLOCKS_CLI:-"npx kb"}
 KBLOCKS_PUBSUB_HOST="${KBLOCKS_PUBSUB_HOST:-}"
 KBLOCKS_API_KEY="${KBLOCKS_API_KEY:-}"
 KBLOCKS_STORAGE_PREFIX="${KBLOCKS_STORAGE_PREFIX:-}"
+KBLOCKS_ACCESS="${KBLOCKS_ACCESS:-}"
 
 context=$(kubectl config current-context)
 
 if [ "$context" == "kind-kind" ]; then
   KBLOCKS_PUBSUB_HOST=portal-redis.default.svc.cluster.local
   KBLOCKS_API_KEY="pass1234"
+  KBLOCKS_ACCESS="write"
 fi
 
 echo "KBLOCKS_PUBSUB_HOST: $KBLOCKS_PUBSUB_HOST"
 echo "KBLOCKS_STORAGE_PREFIX: $KBLOCKS_STORAGE_PREFIX"
+echo "KBLOCKS_ACCESS: $KBLOCKS_ACCESS"
 echo "KBLOCKS_CLI: $KBLOCKS_CLI"
 
 #------------------------------------------------------------------------------#
@@ -51,11 +54,18 @@ install_kblock() {
       KBLOCKS_STORAGE_PREFIX_OPTION=""
     fi
 
+    if [ -n "${KBLOCKS_ACCESS}" ]; then
+      KBLOCKS_ACCESS_OPTION="-e KBLOCKS_ACCESS=${KBLOCKS_ACCESS}"
+    else
+      KBLOCKS_ACCESS_OPTION=""
+    fi
+
     # Now run the command with the options included conditionally
     $KBLOCKS_CLI install \
       $KBLOCKS_PUBSUB_HOST_OPTION \
       $KBLOCKS_API_KEY_OPTION \
       $KBLOCKS_STORAGE_PREFIX_OPTION \
+      $KBLOCKS_ACCESS_OPTION \
       -n kblocks \
       --release-name $name
   )
