7b067aa50af165230fc3e53e05655fef6f91600f
Ainvoner <shaia@monada.co>
2024-12-03 10:01:33 +0200

    cleanup

--- gallery/kblocks/cluster/src/commands.cjs
+++ gallery/kblocks/cluster/src/commands.cjs
@@ -1,4 +1,3 @@
-const crypto = require("crypto");
 const fs = require("fs");
 
 const COMMAND_TEMPLATE = `
@@ -30,12 +29,6 @@ function fillTemplate(template, variables) {
   });
 }
 
-// console.log("KBLOCKS_OBJECT", process.env.KBLOCKS_OBJECT);
-// console.log("KBLOCKS_OUTPUTS", process.env.KBLOCKS_OUTPUTS);
-// console.log("KBLOCKS_API_KEY", process.env.KBLOCKS_API_KEY);
-// console.log("KBLOCKS_PUBSUB_HOST", process.env.KBLOCKS_PUBSUB_HOST);
-// console.log("KBLOCKS_PUBSUB_PORT", process.env.KBLOCKS_PUBSUB_PORT);
-
 const object = JSON.parse(fs.readFileSync(process.env.KBLOCKS_OBJECT, "utf8"));
 const outputs = process.env.KBLOCKS_OUTPUTS;
 const adminPassword = process.env.KBLOCKS_API_KEY;
@@ -43,10 +36,6 @@ const redisHost = process.env.KBLOCKS_PUBSUB_HOST;
 const redisPort = process.env.KBLOCKS_PUBSUB_PORT;
 
 const clusterId = object.metadata.name;
-
-// function generatePassword(length = 16) {
-//   return crypto.randomBytes(length).toString("base64").slice(0, length);
-// }
 
 const writeOutputs = (command) => {
   fs.writeFileSync(
@@ -57,70 +46,24 @@ const writeOutputs = (command) => {
   );
 };
 
-// async function createRedisUser(redis, username, password) {
-//   // Construct the ACL command to create the user
-//   const aclCommand = [
-//     "ACL",
-//     "SETUSER",
-//     username,
-//     "on", // Enable the user
-//     `>${password}`, // Set the user's password
-//     "+@all", // Give all permissions (you can restrict these as needed)
-//   ];
-//   // Execute the ACL SETUSER command
-//   return await redis.call(...aclCommand);
-// }
-
 exports.create = async () => {
-  // const password = generatePassword();
-  // Connect to the Redis server
-  // console.log("Connecting to Redis server");
-  // console.log("redisHost", redisHost);
-  // console.log("redisPort", redisPort);
-  // console.log("adminPassword", adminPassword);
-  // const redis = new Redis({
-  //   host: redisHost,
-  //   port: redisPort,
-  //   password: adminPassword,
-  //   username: "admin",
-  // });
-  // try {
-  //   await createRedisUser(redis, clusterId, password);
-  //   console.log(
-  //     `User '${clusterId}' created successfully with password: ${password}`,
-  //   );
+  // todo create a new user in redis per cluster
   const command = fillTemplate(COMMAND_TEMPLATE, {
-    // KBLOCKS_API_KEY: password,
     KBLOCKS_API_KEY: adminPassword,
     KBLOCKS_PUBSUB_HOST: redisHost,
     KBLOCKS_PUBSUB_PORT: redisPort,
     KBLOCKS_SYSTEM_ID: clusterId,
     KBLOCKS_STORAGE_PREFIX: clusterId,
   });
-  console.log(`Creating cluster ${clusterId}`);
-  console.log("command", command);
+  console.log(`Creating a new cluster ${clusterId}`);
+  console.log("generating a 'create cluster' command", command);
   writeOutputs(command);
-  // } catch (error) {
-  //   console.error("Error creating cluster:", error);
-  //   writeOutputs("");
-  //   throw error;
-  // } finally {
-  //   redis.disconnect();
-  // }
 };
 
 exports.deleteUser = async () => {
-  // const redis = new Redis({
-  //   host: redisHost,
-  //   port: redisPort,
-  //   password: adminPassword,
-  //   username: "admin",
-  // });
-  // await redis.call("ACL", "DELUSER", clusterId);
-  // writeOutputs("");
-  console.log(`Cluster ${clusterId} deleted successfully`);
+  console.log(`Cluster ${clusterId} was deleted successfully`);
 };
 
 exports.update = async () => {
-  console.log(`Cluster ${clusterId} updated successfully`);
+  console.log(`Cluster ${clusterId} was updated successfully`);
 };
