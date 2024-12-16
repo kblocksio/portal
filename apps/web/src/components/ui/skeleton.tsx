32d7321a1f138e93b9ef6d1077a7af0ffe7d681c
Cristian Pallarés <cristianp@wing.cloud>
2024-12-16 10:43:45 +0100

    clusters table loading state

--- apps/web/src/components/ui/skeleton.tsx
+++ apps/web/src/components/ui/skeleton.tsx
@@ -1,15 +1,13 @@
 import { cn } from "@/lib/utils";
 
-function Skeleton({
+export function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("animate-pulse rounded-md bg-muted", className)}
+      className={cn("bg-muted animate-pulse rounded-md", className)}
       {...props}
     />
   );
 }
-
-export { Skeleton };
