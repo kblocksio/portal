8d1ddced31510ae4323af3d6726280ae6f2d05ed
Cristian Pallarés <cristianp@wing.cloud>
2025-01-14 15:38:53 +0100

    split chunks by vendor size

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -5,6 +5,44 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import dotenv from "dotenv";
 dotenv.config();
 
+// These packages are big and we want to split them into their own chunks.
+const bigVendors = [
+  "parse5",
+  "@swagger-api",
+  "swagger-ui-react",
+  "swagger-client",
+  "highlight.js",
+  "@xyflow",
+  "lodash",
+  "@tsparticles",
+  "framer-motion",
+  "refractor",
+  "lucide-react",
+  "@heroicons/react",
+  "@tanstack",
+  "@radix-ui",
+  "remarkable",
+  "autolinker",
+  "react-syntax-highlighter",
+  "zod",
+  "immutable",
+  "linkifyjs",
+  "js-yaml",
+  "dompurify",
+  "tailwind-merge",
+  "ramda",
+  "react-dom",
+  "core-js-pure",
+  "micromark",
+  "micromark-core-components",
+  "micromark-core-commmonmark",
+  "@floating-ui",
+  "@trpc",
+  "entities",
+  "react-use-websocket",
+  "short-unique-id",
+];
+
 export default defineConfig(({ mode }) => ({
   plugins: [TanStackRouterVite(), reactPlugin(), tsconfigPaths()],
   assetsInclude: ["**/*.md"],
@@ -24,5 +62,33 @@ export default defineConfig(({ mode }) => ({
             },
           }
         : undefined,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // manualChunks: {
+        //   // "swagger-ui": ["swagger-ui-react"],
+        //   icons: ["lucide-react", "@heroicons/react"],
+        // },
+        manualChunks(id) {
+          for (const vendor of bigVendors) {
+            if (id.includes(`/node_modules/${vendor}`)) {
+              return vendor;
+            }
+          }
+
+          // if (
+          //   id.includes("/node_modules/lucide-react") ||
+          //   id.includes("/node_modules/@heroicons/react")
+          // ) {
+          //   return "icons";
+          // }
+
+          if (id.includes("/node_modules/")) {
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 }));
